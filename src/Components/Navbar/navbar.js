fc3c1deb1e3442fbc7daa0bbfc7d3929cd5bd33a
Parin Thalangdee <lotteman10@gmail.com>
2022-08-16 17:19:50 -0700

    Added CV to navbar

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -8,6 +8,7 @@ import CodeIcon from '@mui/icons-material/Code';
 import EmailIcon from '@mui/icons-material/EmailSharp'; 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
 // import { color } from '@mui/system';
 
 // const pages = ['About', 'Contact', 'Projects'];
@@ -149,6 +150,11 @@ const ResponsiveAppBar = () => {
             <a href='https://github.com/ParinThalangdee'>
               <GitHubIcon sx={{ color: 'white'}}>
               </GitHubIcon>
+            </a>
+
+            <a href='../../Assets/Documents/ParinThalangdee.pdf' download>
+              <AssignmentSharpIcon sx={{ color: 'white' }}>
+              </AssignmentSharpIcon>
             </a>
           </Box>
 
