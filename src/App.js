4a87139e35e0c1e6ff8276b4ee714182f9e994ba
Parin Thalangdee <lotteman10@gmail.com>
2022-08-17 20:31:35 -0700

    Revision to footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import PageLand from './Components/PageLand';
 import About from './Components/About';
 import Tech from './Components/Tech';
 import Projects from './Components/Projects';
-// import Footer from './Components/Footer'
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       <About />
       <Tech />
       <Projects />
-      {/* <Footer /> */}
+      {/* <Contact /> */}
+      <Footer />
     </>
   );
 }
--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,30 +1,36 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import Box from '@mui/material/Box';
+import EmailIcon from '@mui/icons-material/EmailSharp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-// import './footer.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
 
 const footer = () => {
   return (
     <>
-      <Grid className="footer" container justifyContent="center" spacing={4} padding={2}>
-        <Grid item xs={12} md={2}>
-          <a href="https://github.com/ParinThalangdee" target="_blank">
-            <GitHubIcon></GitHubIcon>
-          </a>
+      <Grid container justifyContent="center" padding={3}>
+          <Grid item xs={2}>
+            <a href='mailto:Parin.Thalangdee@outlook.com' title="Email Me">
+              <EmailIcon></EmailIcon>
+            </a>
+          </Grid>
+          <Grid item xs={2}>
+            <a href='https://www.linkedin.com/in/parinthalangdee/' title="LinkedIn Profile" target='_blank'>
+              <LinkedInIcon></LinkedInIcon>
+            </a>
+          </Grid>
+          <Grid item xs={2}>
+            <a href='https://github.com/ParinThalangdee' title="Github Account" target='_blank'>
+              <GitHubIcon></GitHubIcon>
+            </a>
+          </Grid>
+          <Grid item xs={2}>
+            <a href='../../Assets/Documents/ParinThalangdee.pdf' title="Download Resume" download>
+              <AssignmentSharpIcon></AssignmentSharpIcon>
+            </a>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={2}>
-          <a href="https://www.linkedin.com/in/parinthalangdee/" target="_blank">
-            <LinkedInIcon></LinkedInIcon>
-          </a>
-        </Grid>
-        <Grid item xs={12} md={2}>
-          <a href="https://www.instagram.com/lotteman" target="_blank">
-            <InstagramIcon></InstagramIcon>
-          </a>
-        </Grid>
-      </Grid>
     </>
   )
 }
