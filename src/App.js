7718f73e84bbbb0df5984baa0f791f6f593b6431
Parin Thalangdee <lotteman10@gmail.com>
2022-08-22 23:41:19 -0700

    Revision pageland to home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Navbar from './Components/Navbar';
-import Home from './Components/PageLand';
+import Home from './Components/Home';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
