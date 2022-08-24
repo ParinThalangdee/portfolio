e2ddf316b64cc86ba51fac9edb795fbe1569c612
Parin Thalangdee <lotteman10@gmail.com>
2022-08-24 01:02:14 -0700

    Revision to route paths in App.js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <CssBaseline />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/projects' element={<Projects />}/>
-        <Route path='/contact' element={<Contact />}/>
+        <Route path='portfolio/' element={<Home />}/>
+        <Route path='portfolio/about' element={<About />}/>
+        <Route path='portfolio/projects' element={<Projects />}/>
+        <Route path='portfolio/contact' element={<Contact />}/>
         <Route path='*' element='' />
       </Routes>
     </Router>
