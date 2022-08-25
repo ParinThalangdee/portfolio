fa4efe085385fc962a9f023b1169f28e5658cf08
Parin Thalangdee <lotteman10@gmail.com>
2022-08-25 16:49:54 -0700

    Revision to footer icons

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import EmailIcon from '@mui/icons-material/EmailSharp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 
 const footer = () => {
   return (
@@ -13,22 +13,22 @@ const footer = () => {
         <Grid item xs={2}></Grid>
           <Grid item xs={2}>
           <a href='mailto:Parin.Thalangdee@outlook.com' title="Email Me" style={{ textDecoration: "none", color: "white" }}>
-              <EmailIcon></EmailIcon>
+              <EmailIcon />
             </a>
           </Grid>
           <Grid item xs={2}>
           <a href='https://www.linkedin.com/in/parinthalangdee/' title="LinkedIn Profile" target='_blank' style={{ textDecoration: "none", color: "white" }}>
-              <LinkedInIcon></LinkedInIcon>
+              <LinkedInIcon />
             </a>
           </Grid>
           <Grid item xs={2}>
           <a href='https://github.com/ParinThalangdee' title="Github Account" target='_blank' style={{ textDecoration: "none", color: "whjte" }}>
-              <GitHubIcon></GitHubIcon>
+              <GitHubIcon />
             </a>
           </Grid>
           <Grid item xs={2}>
           <a href='../../Assets/Documents/ParinThalangdee.pdf' title="Download Resume" download style={{ textDecoration: "none", color: "white" }}>
-              <AssignmentSharpIcon></AssignmentSharpIcon>
+              <AssignmentIcon />
             </a>
           </Grid>
         </Grid>
